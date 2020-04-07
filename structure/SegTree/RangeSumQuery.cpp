5e265961a377c5e7f1cee14103d600460e21de0f
chakku <chakkurai0724@outlook.jp>
2020-04-08 01:39:30 +0900

    add Range Sum Query SegTree

---
+++ structure/SegTree/RangeSumQuery.cpp
@@ -0,0 +1,38 @@
+template<typename T, T iniv=0>
+struct SegTree{
+    int n;
+    vector<T> dat;
+    SegTree(int n_){
+        n = 1;
+        while(n < n_) n *= 2;
+        dat.resize(2*n-1);
+        for(int i=0;i<2*n-1;i++) dat[i] = iniv;
+    }
+
+    void update(int k, T a){
+        k += n-1;
+        dat[k] = a;
+        while(k > 0){
+            k = (k-1)/2;
+            dat[k] = dat[k*2+1] + dat[k*2+2];
+        }
+    }
+
+    T get(int i) const {
+        return dat[i+n-1];
+    }
+
+    T query(int a, int b, int k, int l, int r){
+        if(r<=a or b<=l) return iniv;
+        else if(a<=l and r<=b) return dat[k];
+        else{
+            T vl = query(a,b,k*2+1,l,(l+r)/2);
+            T vr = query(a,b,k*2+2,(l+r)/2,r);
+            return vl+vr;
+        }
+    }
+
+    T query(int a,int b){
+        return query(a,b,0,0,n);
+    }
+};
